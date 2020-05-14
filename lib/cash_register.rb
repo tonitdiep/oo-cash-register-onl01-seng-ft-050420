15ac6972ab79e7887ff4ebfab36d3d8184a917ef
Toni  Diep <adiep123@gmail.com>
2020-05-14 01:31:48 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class CashRegister
-attr_accessor :total, :discount, :last_transaction, :items, :title
+attr_accessor :total, :discount, :items, :title
 
   def initialize(discount = 0)
     @total = 0
