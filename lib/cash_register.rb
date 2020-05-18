8820005bff95ec2c0318a2b6704bacf9a83d024b
Toni  Diep <adiep123@gmail.com>
2020-05-18 04:18:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class CashRegister
 attr_accessor :total, :discount, :items, :title
-#didn't use last_transaction 
+#don't use last_transaction here ^
   def initialize(discount = 0)
     @total = 0
     @discount = discount
